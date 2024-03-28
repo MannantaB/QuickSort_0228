912b1ae73419a1c577825ded8420d16c2cb9ff4e
MannantaB <mannanta18@gmail.com>
2024-03-28 15:02:50 +0700

    deklarasi variabel movements count

--- pertemuankeenam/pertemuankeenam.cpp
+++ pertemuankeenam/pertemuankeenam.cpp
@@ -4,6 +4,15 @@ using namespace std;
 //array if integers to hold values
 int arr[20];
 int cmp_count = 0; //number of comparison
+int mov_count = 0; //number of data movements
+
+
+
+
+
+
+
+
 
 int main()
 {
