9d83b708953cabc0148260edd3c351b2544875df
MannantaB <mannanta18@gmail.com>
2024-03-28 15:37:59 +0700

    create prosedur display

--- pertemuankeenam/pertemuankeenam.cpp
+++ pertemuankeenam/pertemuankeenam.cpp
@@ -83,7 +83,19 @@ void q_short(int low, int high)
     q_short(j + 1, high); //step 13
 }
 
+void display() {
+    cout << "\n--------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "\n--------------" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparisons: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
 
 
 
